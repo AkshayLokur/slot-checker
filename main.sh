1ae8c796f02a8e29a4d18150766a65cfbcf1865d
Akshay Lokur <alokur@apple.com>
2020-04-21 22:41:09 +0800

    Adding PATH env variable in sh file

--- main.sh
+++ main.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+export PATH=/usr/local/bin/:$PATH
 printf '%s\t' "$(date +'%Y%m%d-%H:%M:%S.%3N')"
 printf "Slot checker run start...\n\n"
 source /Users/akshaylokur/Work/study/slot-checker/venv/bin/activate
 python /Users/akshaylokur/Work/study/slot-checker/main.py
+deactivate
 printf '%s\t' "$(date +'%Y%m%d-%H:%M:%S.%3N')"
 printf "Slot checker run finished.\n\n"
-deactivate
