89dfe887f4052a04a3bd95cd10697512c72b8813
Akshay Lokur <alokur@apple.com>
2020-04-21 19:38:27 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Slot checker:
+
+Chrome driver:
+
+Add chrome driver to classpath - e.g. /usr/local/bin on Mac:
+
+https://sites.google.com/a/chromium.org/chromedriver/downloads
+https://chromedriver.storage.googleapis.com/index.html?path=81.0.4044.69/
+
+
+
---
+++ main.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+import sys
+from subprocess import call
+
+if __name__ == "__main__":
+    browser = webdriver.Chrome() # Add chrome driver to classpath - e.g. /usr/local/bin on Mac
+    browser.get('https://www.waangoo.com/deliveryslotdetails')
+    slots = browser.find_element_by_xpath("//td[@style='text-align: center;']")
+    if slots is not None:
+        # Increase Mac volume to 100%
+
+        call(["osascript -e 'set volume output volume 100'"], shell=True)
+        audio_file = '/System/Library/Sounds/Funk.aiff'
+        for i in range(50):
+            return_code = call(["afplay", audio_file]) # Works on Mac
+
