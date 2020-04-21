2b2568c20c5701652003eac544f77393f76eecca
Akshay Lokur <alokur@apple.com>
2020-04-21 19:42:15 +0800

    First commit

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
     slots = browser.find_element_by_xpath("//td[@style='text-align: center;']")
     if slots is not None:
         # Increase Mac volume to 100%
-
         call(["osascript -e 'set volume output volume 100'"], shell=True)
         audio_file = '/System/Library/Sounds/Funk.aiff'
         for i in range(50):
             return_code = call(["afplay", audio_file]) # Works on Mac
+            # print('\a\a\a\a\a\a\a\a\a') # For windows may be
 
