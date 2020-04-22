8f1c48f770d87af6fbf5ce7817b9e34ccaa21fcc
Akshay Lokur <alokur@apple.com>
2020-04-22 10:24:27 +0800

    Adding sleep

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from selenium import webdriver
 import sys
 from subprocess import call
+import time
 
 if __name__ == "__main__":
     with webdriver.Chrome() as browser:  # Add chrome driver to classpath - e.g. /usr/local/bin on Mac
         browser.get("https://www.waangoo.com/deliveryslotdetails")
+        time.sleep(1)
         try:
-            print('\tTrying to find open slot...\n')
+            print("\tTrying to find open slot...\n")
             slots = browser.find_element_by_xpath("//td[@style='text-align: center;']")
             if slots is not None:
-                print('\tFound open slot!\n')
+                print("\tFound open slot!\n")
                 # Increase Mac volume to 100%
                 call(["osascript -e 'set volume output volume 100'"], shell=True)
                 audio_file = "/System/Library/Sounds/Funk.aiff"
